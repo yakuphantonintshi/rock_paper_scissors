098aecb1d4153b299515b9aad89f966005e26876
Yakupha Ntonintshi <yantocpt025@student.wethinkcode.co.za>
2025-12-23 21:30:47 +0200

    Donepython ./rockpaperscissors.py

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -2,17 +2,32 @@ import random
 
 
 while True:
-    options = ["r", "p", "s"]
+    options = ("r", "p", "s")
     computer_choice = random.choice(options)
     choice = input("Rock, Paper, Scissors? (r/p/s): ").lower()
 
     if choice not in options:
         print(f"Computer choice: {computer_choice}")
         print("Not valid")
-    elif choice != computer_choice:
+    elif choice == "r" and computer_choice == "p":
         print(f"Computer choice: {computer_choice}")
-        print("You lose!")
+        print("Computer Won!")
+    elif choice == "r" and computer_choice == "s":
+        print(f"Computer choice: {computer_choice}")
+        print("You Won!")
+    elif choice == "s" and computer_choice == "r":
+        print(f"Computer choice: {computer_choice}")
+        print("Computer Won!")
+    elif choice == "s" and computer_choice == "p":
+        print(f"Computer choice: {computer_choice}")
+        print("You Won!")
+    elif choice == "p" and computer_choice == "s":
+        print(f"Computer choice: {computer_choice}")
+        print("Computer Won!")
+    elif choice == "p" and computer_choice == "r":
+        print(f"Computer choice: {computer_choice}")
+        print("You Won!")
     else:
         print(f"Computer choice: {computer_choice}")
-        print("You win!")
+        print("It's a Tie!")
         break
